bfa92891cf49a1de20181cac1189a429ed147aba
Hugo Pérez <hugo.perez.sge@mango.com>
2022-11-01 10:09:53 +0100

    starting project

---
+++ src/test/java/com/hugoperez/tmc/TmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugoperez.tmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
